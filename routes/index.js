5a37170e5e8685ace6a575828a15b7f5c1b42379
Dario <derioreal63@gmail.com>
2024-04-05 19:32:30 +0200

    implement new mod system

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,93 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const axios = require('axios');
+const cheerio = require('cheerio');
+require('dotenv').config();
 
-router.get('/mod/:jarName', (req, res) => {
-  const jarName = req.params.jarName;
-  const filePath = "mods/" + jarName;
+router.get('/latest/:name', async (req, res) => {
+  const name = req.params.name;
 
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).send('Jar file not found');
+  const response = await fetch(`https://api.github.com/users/Knerio/packages/maven/de.derioo.mods.${name}/versions`, {
+    headers: {
+      'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN,
+    }
+  });
+
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch packages');
   }
 
-  res.setHeader('Content-Disposition', `attachment; filename="${jarName}"`);
-  res.setHeader('Content-Type', 'application/java-archive');
+  const data = await response.json();
+  let latest = null;
+  let latestUpdate = 0;
 
-  const fileStream = fs.createReadStream(filePath);
-  fileStream.pipe(res);
-});
 
-router.get('/mods', (req, res) => {
-  fs.readdir("./mods", (err, files) => {
-    if (err) {
-      return res.status(500).send('Error reading directory');
+  for (let i = 0; i < data.length; i++) {
+    const updated = new Date(data[i]["updated_at"]).getTime();
+    if (latest === null) {
+      latest = data[i].name;
+      latestUpdate = updated;
+      continue
+    }
+    if (latestUpdate > updated) {
+      latest = data[i].name;
+      latestUpdate = updated
     }
 
-    res.json(files
-        .filter(file => file.endsWith(".jar"))
-        .map(fileName => {
-          return fileName.replace(".jar", "");
-        }));
+  }
+  console.log(name)
+  console.log(latest)
+  const url = `https://maven.pkg.github.com/Knerio/Simple-Chals-Server/de.derioo.mods.${name}/${latest}/${name}-${latest}-dev.jar`
+
+  const response2 = await axios({
+    url: url,
+    method: 'GET',
+    responseType: 'arraybuffer',
+    headers: {
+      "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
+    }
   });
-})
+
+
+
+  res.setHeader('Content-Disposition', `attachment; filename="${name}.jar"`);
+  res.setHeader('Content-Type', 'application/java-archive');
+
+  res.send(response2.data);
+});
+
+router.get('/mods', async (req, res) => {
+  try {
+
+    const response = await fetch(`https://api.github.com/users/Knerio/packages?package_type=maven`, {
+      headers: {
+        'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN,
+      }
+    });
+
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch packages');
+    }
+
+    const data = await response.json();
+    const jsonResponse = [];
+
+
+    for (let i = 0; i < data.length; i++) {
+      const name = data[i].name;
+      if (!name.startsWith("de.derioo.mods.")) continue;
+      jsonResponse.push(name.replace("de.derioo.mods.", ""))
+    }
+
+    res.json(jsonResponse);
+  } catch (error) {
+    console.error('Error fetching packages:', error);
+    res.status(500).json({ error: 'Failed to fetch packages' });
+  }
+});
 
 
 module.exports = router;
