c1e993034a0519dcd71e052264a92040a73ed5a1
Dario <derioreal63@gmail.com>
2024-04-06 14:37:43 +0200

    cleanup project and remove axios fetch

--- routes/index.js
+++ routes/index.js
@@ -1,89 +1,78 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const {Readable} = require("stream")
 require('dotenv').config();
 
 router.get('/latest/:name', async (req, res) => {
-  const name = req.params.name;
+    const name = req.params.name;
 
-  const response = await fetch(`https://api.github.com/users/Knerio/packages/maven/de.derioo.mods.${name}/versions`, {
-    headers: {
-      'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN,
-    }
-  });
-
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch packages');
-  }
-
-  const data = await response.json();
-  let latest = null;
-  let latestUpdate = 0;
-
-
-  for (let i = 0; i < data.length; i++) {
-    const updated = new Date(data[i]["updated_at"]).getTime();
-    if (latest === null) {
-      latest = data[i].name;
-      latestUpdate = updated;
-      continue
-    }
-    if (latestUpdate < updated) {
-      latest = data[i].name;
-      latestUpdate = updated
-    }
-
-  }
-
-  const url = `https://maven.pkg.github.com/Knerio/Simple-Chals-Server/de.derioo.mods.${name}/${latest}/${name}-${latest}.jar`
-
-  const response2 = await axios({
-    url: url,
-    method: 'GET',
-    responseType: 'arraybuffer',
-    headers: {
-      "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
-    }
-  });
-
-
-
-  res.setHeader('Content-Disposition', `attachment; filename="${name}.jar"`);
-  res.setHeader('Content-Type', 'application/java-archive');
-
-  res.send(response2.data);
-});
-
-router.get('/mods', async (req, res) => {
-  try {
-
-    const response = await fetch(`https://api.github.com/users/Knerio/packages?package_type=maven`, {
-      headers: {
-        'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN,
-      }
+    const response = await fetch(`https://api.github.com/users/Knerio/packages/maven/de.derioo.mods.${name}/versions`, {
+        headers: {
+            'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN,
+        }
     });
 
 
     if (!response.ok) {
-      throw new Error('Failed to fetch packages');
+        throw new Error('Failed to fetch packages');
     }
 
     const data = await response.json();
-    const jsonResponse = [];
+    const latestPackage = data.reduce((acc, item) => {
+        const updatedItem = new Date(item["updated_at"]).getTime();
+        const updatedAcc = new Date(acc["updated_at"]).getTime();
+        if (updatedAcc > updatedItem) return acc;
+        return item;
+    }, {"updated_at": 0});
+
+    console.log(latestPackage.name)
+    const latestJarURL = `https://maven.pkg.github.com/Knerio/Simple-Chals-Server/de.derioo.mods.${name}/${latestPackage.name}/${name}-${latestPackage.name}.jar`
+
+    const jarResponse = await fetch(latestJarURL, {
+        method: 'GET',
+        responseType: 'blob',
+        headers: {
+            "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
+        }
+    })
 
 
-    for (let i = 0; i < data.length; i++) {
-      const name = data[i].name;
-      if (!name.startsWith("de.derioo.mods.")) continue;
-      jsonResponse.push(name.replace("de.derioo.mods.", ""))
+    res.setHeader('Content-Disposition', `attachment; filename="${name}.jar"`);
+    res.setHeader('Content-Type', 'application/java-archive');
+    Readable.fromWeb(jarResponse.body).pipe(res);
+
+});
+
+router.get('/mods', async (req, res) => {
+    try {
+
+        const response = await fetch(`https://api.github.com/users/Knerio/packages?package_type=maven`, {
+            headers: {
+                'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN,
+            }
+        });
+
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch packages');
+        }
+
+        const data = await response.json();
+        const jsonResponse = [];
+
+
+        for (let i = 0; i < data.length; i++) {
+            const name = data[i].name;
+            if (!name.startsWith("de.derioo.mods.")) continue;
+            jsonResponse.push(name.replace("de.derioo.mods.", ""))
+        }
+
+        res.json(jsonResponse);
+    } catch (error) {
+        console.error('Error fetching packages:', error);
+        res.status(500).json({error: 'Failed to fetch packages'});
     }
-
-    res.json(jsonResponse);
-  } catch (error) {
-    console.error('Error fetching packages:', error);
-    res.status(500).json({ error: 'Failed to fetch packages' });
-  }
 });
 
 
